c974b9d962902f9d6c9e7a295a506e006fecb724
“Edward <eminaya49@gmail.com>
2023-04-11 10:51:57 -0500

    completed heck_target class. Need to create method to determine when game is won

--- check_target.rb
+++ check_target.rb
@@ -1,34 +1,21 @@
-def check_guess(target, guess)
-  target = target.downcase # Convert both target and guess to lowercase for case-insensitive comparison
-  guess = guess.downcase
+class CheckTarget
+  def check_guess(target, guess)
+    target_copy = target.chars.map { |char| guess.include?(char) ? char : nil }
 
-  # Create a copy of target with characters to be matched replaced with nil
-  target_copy = target.chars.map { |char| guess.include?(char) ? char : nil }
-
-  # Compare each character of guess with target_copy and update result array
-  result = []
-  guess.chars.each_with_index do |char, i|
-    if char == target[i]
-      result << "exact match"
-      target_copy[i] = nil # Set to nil to avoid counting duplicates
-    elsif target_copy.include?(char)
-      result << "partial match"
-      target_copy[target_copy.index(char)] = nil # Set to nil to avoid counting duplicates
-    else
-      result << "no match"
+    # Compare each character of guess with target_copy and update result array
+    result = ""
+    guess.chars.each_with_index do |char, i|
+      if char == target[i]
+        result += "\e[32m#{char}\e[0m"
+        target_copy[i] = nil # Set to nil to avoid counting duplicates
+      elsif target_copy.include?(char)
+        result += "\e[33m#{char}\e[0m"
+        target_copy[target_copy.index(char)] = nil # Set to nil to avoid counting duplicates
+      else
+        result += "#{char}"
+      end
     end
+
+    return result
   end
-
-  return result
 end
-
-# Example usage
-target = "hello"
-guess = "helpl"
-result = check_guess(target, guess)
-puts result.join(", ") # Output: exact match, no match, no match, partial match, no match
-
-target = "hello"
-guess = "heeps"
-result = check_guess(target, guess)
-puts result.join(", ") # Output: exact match, no match, no match, partial match, no match
--- wordle.rb
+++ wordle.rb
@@ -1,69 +1,27 @@
 require "./word_list.rb"
 require "./guesses.rb"
+require "./check_target.rb"
 
 file = WordList.new
 word_list = file.word_list("words.txt")
 target = file.generate_word(word_list)
 
-# check users guess against choosen word
-def check_users_guess(array, input, target)
-  array_game = game_word.split("")
-  array_input = input.split("")
-  game_input = array_game.zip(array_input)
-  word = []
-  game_input.each do |pair|
-    x = []
-    if pair[0] == pair[1]
-      x << pair[1]
-      x << "yes"
-      word << x
-    elsif game_word.index(pair[1]) != nil
-      x << pair[1]
-      x << "close"
-      word << x
-    else
-      x << pair[1]
-      x << "no"
-      word << x
-    end
-  end
-  array << word
-  puts "----------"
-end
-
-# Display Entries
-def display(entries)
-  entries.each do |word|
-    string = ""
-    word.each do |letter|
-      if letter[1] == "yes"
-        string += "\e[32m#{letter[0]}\e[0m"
-      elsif letter[1] == "close"
-        string += "\e[33m#{letter[0]}\e[0m"
-      else
-        string += letter[0]
-      end
-    end
-    puts string
-  end
-end
-
-# Asking user for guesses
+# GAME
 all_entries = []
 while true
+  system("clear")
+  all_entries.each do |word|
+    puts word
+  end
   input = Guesses.new.prompt(word_list)
   if input[1]
-    all_entries << input[0]
+    check = CheckTarget.new
+    result = check.check_guess(target, input[0])
+    all_entries << result
   end
-  # word_list.each do |word|
-  #   if word == input
-  #     valid_word = true
-  #     check_users_guess(all_entries, input, target)
-  #     display(all_entries)
-  #   end
-  # end
-  # if valid_word && all_entries.length == 5
-  #   break
-  # end
+
+  if all_entries.length == 5
+    break
+  end
 end
-puts game_word
+puts target
